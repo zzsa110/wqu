fe20690d1fad2ffb76d7c6a3709ee752e0a69c95
zzsa110 <163087273+zzsa110@users.noreply.github.com>
2024-10-17 18:46:26 +0800

    Update scsj.sh

--- scsj.sh
+++ scsj.sh
@@ -24,7 +24,7 @@ benjina=$(hostname)
 #urll='{"name":"'"$benjina"'","id":"'"$api_key"'","quil":"'"$QUIL"'","ver":"'"$Ver"'","time":"'"$timestamp"'"}'
 #url="http://192.168.1.107:7001/ip=$urll"  
 #1号1.107,2号1.251
-url="http://192.168.1.251:7001/ip=$benjina----$api_key----$QUIL----$Ver----$timestamp"
+url="http://192.168.1.107:7001/ip=$benjina----$api_key----$QUIL----$Ver----$timestamp"
 #echo $url
 
 
